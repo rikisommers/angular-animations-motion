0810adb80bceb82769560b27b591410f77512f43
rikisommers <rikisommers@yahoo.co.nz>
2025-02-11 22:13:13 +1300

    init

---
+++ projects/ngx-motion/src/lib/ngx-motion.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { NgxMotionDirective } from './ngx-motion.directive';
+
+@NgModule({
+  declarations: [
+    NgxMotionDirective
+  ],
+  imports: [
+  ],
+  exports: [
+    NgxMotionDirective
+  ]
+})
+export class NgxMotionModule { }
